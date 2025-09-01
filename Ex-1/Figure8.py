dfb097a99ad43b1e8cf3c450cba1a2a9368c6386
Andreas Kærlev <bzd796@alumni.ku.dk>
2025-09-01 16:46:01 +0200

    Figure8imrpvoe

--- Ex-1/Figure8.py
+++ Ex-1/Figure8.py
@@ -12,17 +12,17 @@ def PerformFigureEight():
     sleep(2)
     # Start Turn
     print(arlo.stop())
-    print(arlo.go_diff(41, 127, 0, 1))
+    print(arlo.go_diff(41, 127, 1, 1))
 
-    sleep(3.5)
+    sleep(2)
     # Go Straight
     print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
 
     sleep(2)
     # Start Turn
-    print(arlo.go_diff(127, 41, 1, 0))
+    print(arlo.go_diff(127, 41, 1, 1))
 
-    sleep(3.5)
+    sleep(2)
     # Should be at start position now
     print(arlo.stop())
 
