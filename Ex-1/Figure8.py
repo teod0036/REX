3be217685ecf4a9db85edc90f565212f1c8ea2b2
Andreas Kærlev <bzd796@alumni.ku.dk>
2025-09-01 16:52:34 +0200

    FixedFixed

--- Ex-1/Figure8.py
+++ Ex-1/Figure8.py
@@ -18,7 +18,7 @@ def PerformFigureEight():
     # Go Straight
     print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
 
-    sleep(2)
+    sleep(1.7)
     # Start Turn
     print(arlo.go_diff(127, 41, 1, 1))
 
