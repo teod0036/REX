7beb8f24814ff064cdaddce46f7576fa7b342f5b
Andreas Kærlev <bzd796@alumni.ku.dk>
2025-09-03 12:56:32 +0200

    idk

---
+++ Ex-1/Turn90.py
@@ -0,0 +1,16 @@
+from time import sleep
+import robot
+
+arlo = robot.Robot()
+
+def perform_Turn90():
+
+    # Speed constants
+    leftSpeed = 64  
+    rightSpeed = 64
+
+    print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
+
+
+
+perform_Turn90()
