a98416ca691ad4e8754cba8385824df6d83c749f
Andreas Kærlev <bzd796@alumni.ku.dk>
2025-09-03 13:42:19 +0200

    fixed

---
+++ Ex-1/Square.py
@@ -0,0 +1,16 @@
+from time import sleep
+import robot
+from oneMeter import go_meter
+from Turn90 import perform_Turn90
+
+arlo = robot.Robot()
+
+def perform_square():
+    for i in range(4):
+        #go straight
+        go_meter()
+
+        #turn right
+        perform_Turn90()
+
+perform_square()
