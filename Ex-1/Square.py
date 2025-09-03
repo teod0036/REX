0bf443ceb1da6d93862e30ed7e11924c5b2e924b
Andreas Kærlev <bzd796@alumni.ku.dk>
2025-09-03 13:36:41 +0200

    Fixedsquare

--- Ex-1/Square.py
+++ Ex-1/Square.py
@@ -1,24 +1,16 @@
 from time import sleep
 import robot
 from oneMeter import go_meter
+from Turn90 import perform_Turn90
 
 arlo = robot.Robot()
 
 def perform_square():
-    # Time constants
-    turn_sleep = 2
-
-    # Speed constants
-    leftSpeed = 50
-    rightSpeed = 50
-
     for i in range(4):
         #go straight
         go_meter()
 
         #turn right
-        print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
-        sleep(turn_sleep)
-
+        perform_Turn90
 
 perform_square()
