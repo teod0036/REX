70f80b35ad610fa784c208ef132397d6f675bfa0
Andreas Kærlev <bzd796@alumni.ku.dk>
2025-09-03 14:23:57 +0200

    test123

--- Ex-1/Circletest.py
+++ Ex-1/Circletest.py
@@ -3,7 +3,7 @@ import robot
 from oneMeter import go_meter
 arlo = robot.Robot()
 
-def PerformTurnRightCircle():
+def PerformTurnLeftCircle():
     # Go Straight
     leftSpeed = 64
     rightSpeed = 64
@@ -12,12 +12,12 @@ def PerformTurnRightCircle():
     print(arlo.go_diff(43, 114 + Rightspeedmodifier, 1, 1))
     sleep(turntimer)
 
-def PerformTurnLeftCircle():
+def PerformTurnRightCircle():
     leftSpeed = 64
     rightSpeed = 64
     Rightspeedmodifier = -2
     turntimer = 3.2
-    print(arlo.go_diff(114, 43 + Rightspeedmodifier, 1, 1))
+    print(arlo.go_diff(114, 43, 1, 1))
     sleep(turntimer)
 
 
