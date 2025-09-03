79b912b981dd95db7987a26f98d9f726deb76f2c
Andreas Kærlev <bzd796@alumni.ku.dk>
2025-09-03 13:28:05 +0200

    Test90

--- Ex-1/Turn90.py
+++ Ex-1/Turn90.py
@@ -9,20 +9,16 @@ def perform_Turn90(withclock: bool, sleepduration: float = 0.685):
     # Speed constants
     leftSpeed = 64  
     rightSpeed = 64
+    rightSpeedmodifier = -2
     if withclock:
-        print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
+        print(arlo.go_diff(leftSpeed, rightSpeed + rightSpeedmodifier, 1, 0))
     else:
-        print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
-        sleep(sleepduration / 50)
+        print(arlo.go_diff(leftSpeed, rightSpeed + rightSpeedmodifier, 0, 1))
 
     sleep(sleepduration)
 
 
 
-perform_Turn90(False)
-perform_Turn90(False)
-perform_Turn90(False)
-perform_Turn90(False)
 perform_Turn90(False)
 perform_Turn90(False)
 perform_Turn90(False)
