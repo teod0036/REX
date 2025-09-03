96370cc1d6277e19c98e31eabd54e9cd206d7649
Andreas Kærlev <bzd796@alumni.ku.dk>
2025-09-03 14:36:01 +0200

    test123

--- Ex-1/Circletest.py
+++ Ex-1/Circletest.py
@@ -7,15 +7,14 @@ def PerformTurnLeftCircle():
     # Go Straight
     leftSpeed = 64
     rightSpeed = 64
-    Rightspeedmodifier = 0
+    Rightspeedmodifier = -5
     turntimer = 3.2
-    print(arlo.go_diff(0, 96, 1, 1))
+    print(arlo.go_diff(0, 96 + Rightspeedmodifier, 1, 1))
     sleep(turntimer)
 
 def PerformTurnRightCircle():
     leftSpeed = 64
     rightSpeed = 64
-    Rightspeedmodifier = -2
     turntimer = 3.2
     print(arlo.go_diff(96, 0, 1, 1))
     sleep(turntimer)
