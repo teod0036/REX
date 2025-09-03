e687af1a799e18ad4050b5a44f8a8016d933a734
Andreas Kærlev <bzd796@alumni.ku.dk>
2025-09-03 13:20:45 +0200

    Testtest

--- Ex-1/TestWheels.py
+++ Ex-1/TestWheels.py
@@ -9,8 +9,9 @@ def TestWheels(withclock: bool, sleepduration: float = 0.5):
     # Speed constants
     leftSpeed = 64  
     rightSpeed = 64
+    Rightspeedmodifier = -1
     if withclock:
-        print(arlo.go_diff(0, rightSpeed, 1, 1))
+        print(arlo.go_diff(0, rightSpeed + Rightspeedmodifier, 1, 1))
     else:
         print(arlo.go_diff(leftSpeed, 0, 1, 1))
 
