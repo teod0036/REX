d107a8bd7746b64dddd18c9588154b42b532b0d9
Andreas Kærlev <bzd796@alumni.ku.dk>
2025-09-03 14:09:28 +0200

    test123

--- Ex-1/Figure8.py
+++ Ex-1/Figure8.py
@@ -8,20 +8,21 @@ def PerformFigureEight():
     leftSpeed = 64
     rightSpeed = 64
     Rightspeedmodifier = -2
+    turntimer = 3.1
     go_meter()
 
     # Start Turn
     print(arlo.stop())
     print(arlo.go_diff(43, rightSpeed + 50 + Rightspeedmodifier, 1, 1))
 
-    sleep(3)
+    sleep(turntimer)
     # Go Straight
 
     go_meter()
     # Start Turn
     print(arlo.go_diff(leftSpeed + 50, 43 + Rightspeedmodifier, 1, 1))
 
-    sleep(3)
+    sleep(turntimer)
     # Should be at start position now
     print(arlo.stop())
 
