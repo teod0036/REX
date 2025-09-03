e5532e7d188deb7c21c0bfcaf36bb6c9db791fc1
Andreas Kærlev <bzd796@alumni.ku.dk>
2025-09-03 14:04:08 +0200

    test123

--- Ex-1/Figure8.py
+++ Ex-1/Figure8.py
@@ -1,30 +1,29 @@
 from time import sleep
 import robot
-
+from oneMeter import go_meter
 arlo = robot.Robot()
 
 def PerformFigureEight():
     # Go Straight
     leftSpeed = 64
     rightSpeed = 64
-    print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
+    Rightspeedmodifier = -2
+    go_meter()
 
-    sleep(2)
     # Start Turn
     print(arlo.stop())
-    print(arlo.go_diff(41, 127, 1, 1))
+    print(arlo.go_diff(43, 127 + Rightspeedmodifier, 1, 1))
 
     sleep(2.5)
     # Go Straight
-    print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
 
-    sleep(1.7)
+    go_meter()
     # Start Turn
-    print(arlo.go_diff(127, 41, 1, 1))
+    print(arlo.go_diff(127, 43 + Rightspeedmodifier, 1, 1))
 
-    sleep(3.25)
+    sleep(2.5)
     # Should be at start position now
     print(arlo.stop())
 
-PerformFigureEight()
+
 PerformFigureEight()
