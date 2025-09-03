34aa3435f7f61afcc31888b44a1a0d0304e061cd
Andreas Kærlev <bzd796@alumni.ku.dk>
2025-09-03 13:31:39 +0200

    Fluer

--- Ex-1/Turn90.py
+++ Ex-1/Turn90.py
@@ -19,7 +19,7 @@ def perform_Turn90(withclock: bool, sleepduration: float = 0.694):
 
 
 
-perform_Turn90(True)
-perform_Turn90(True)
-perform_Turn90(True)
-perform_Turn90(True)
+perform_Turn90(False)
+perform_Turn90(False)
+perform_Turn90(False)
+perform_Turn90(False)
