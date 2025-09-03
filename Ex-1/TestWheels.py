387a3122150f0dc8b9788c40f192068b242ef7e0
Andreas Kærlev <bzd796@alumni.ku.dk>
2025-09-03 15:33:23 +0200

    123123

--- Ex-1/TestWheels.py
+++ Ex-1/TestWheels.py
@@ -6,7 +6,7 @@ arlo = robot.Robot()
 def TestWheels(userightwheel: bool, sleepduration: float = 0.5):
         # Speed constants
     speed = 107
-    Rightspeedmodifier = 0
+    Rightspeedmodifier = -5
     if userightwheel:
         print(arlo.go_diff(0, speed + Rightspeedmodifier, 1, 1))
     else:
