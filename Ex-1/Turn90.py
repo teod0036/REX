c8152f6e7b8637d9ecb1defbed993d15f03a413b
Andreas Kærlev <bzd796@alumni.ku.dk>
2025-09-03 13:10:37 +0200

    Betterturn

--- Ex-1/Turn90.py
+++ Ex-1/Turn90.py
@@ -3,24 +3,29 @@ import robot
 
 arlo = robot.Robot()
 
-def perform_Turn90():
+def perform_Turn90(withclock: bool, sleepduration: float = 0.685):
+    
 
     # Speed constants
     leftSpeed = 64  
     rightSpeed = 64
+    if withclock:
+        print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
+    else:
+        print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
 
-    print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
-    sleep(0.685)
+    sleep(sleepduration)
 
 
 
-perform_Turn90()
-perform_Turn90()
-perform_Turn90()
-perform_Turn90()
-perform_Turn90()
-perform_Turn90()
-perform_Turn90()
-perform_Turn90()
+perform_Turn90(False)
+perform_Turn90(False)
+perform_Turn90(False)
+perform_Turn90(False)
+perform_Turn90(False)
+perform_Turn90(False)
+perform_Turn90(False)
+perform_Turn90(False)
+
 
 
