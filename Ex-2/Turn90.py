dadd15eed529e4a521709abf9f4778a6226d1f3d
abxh <83485102+abxh@users.noreply.github.com>
2025-09-08 16:12:14 +0200

    use previous calibration

--- Ex-2/Turn90.py
+++ Ex-2/Turn90.py
@@ -1,18 +1,17 @@
 from time import sleep
 import robot
 
+from rightSpeedModifier import rightSpeedmodifier
+
 arlo = robot.Robot()
 
 def perform_Turn90(withclock: bool, sleepduration: float = 0.694):
-    
-
     # Speed constants
     leftSpeed = 64  
-    rightSpeed = 64
-    rightSpeedmodifier = -2
+    rightSpeed = 64 + rightSpeedmodifier[leftSpeed]
     if withclock:
-        print(arlo.go_diff(leftSpeed, rightSpeed + rightSpeedmodifier, 1, 0))
+        print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
     else:
-        print(arlo.go_diff(leftSpeed, rightSpeed + rightSpeedmodifier, 0, 1))
+        print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
 
     sleep(sleepduration)
