dbdf1320fe5a8b49963095e35913f5f78178d65a
abxh <83485102+abxh@users.noreply.github.com>
2025-09-08 16:21:20 +0200

    changes

---
+++ Ex-2/RotateAndMeasure.py
@@ -0,0 +1,35 @@
+from time import perf_counter, sleep
+
+import robot
+from rightSpeedModifier import rightSpeedmodifier
+
+arlo = robot.Robot()
+
+def perform_Turn(withclock: bool, angle: float):
+    turn90_duration = 0.694 * angle / 90
+
+    leftSpeed = 64  
+    rightSpeed = 64 + rightSpeedmodifier[leftSpeed]
+    if withclock:
+        print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
+    else:
+        print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
+
+    sleep(turn90_duration)
+
+def main(go_time=10):
+    start = perf_counter()
+    isGoing = True
+    while isGoing:  # or some other form of loop
+        if perf_counter() - start > go_time:  # Stop after 5 seconds
+            print(arlo.stop())
+            isGoing = False
+
+for i in range(36):
+    perform_Turn(True, 10)
+
+
+# if __name__ == "__main__":
+#     main()
+
+
