540c7e923c0c40daf8adde6490570d8eb7b03268
Andreas Kærlev <bzd796@alumni.ku.dk>
2025-09-08 15:50:41 +0200

    testsensors

---
+++ Ex-2/TestSensorsOnDistance.py
@@ -0,0 +1,15 @@
+from time import perf_counter
+import robot
+from Turn90 import perform_Turn90
+
+arlo = robot.Robot()
+
+newlist = []
+def test_sensors():
+
+    for i in range(5):
+        resultval = arlo.read_front_ping_sensor()
+        newlist.append(resultval)
+    print(newlist)
+
+test_sensors()
