ca5bdb70365fe3e1bb1788ad70648c964b3a2a77
Andreas Kærlev <bzd796@alumni.ku.dk>
2025-09-15 16:26:26 +0200

    newcap

--- Ex-3/imagecapture.py
+++ Ex-3/imagecapture.py
@@ -1,11 +1,6 @@
-# This script shows how to open a camera the picamera2 module and grab frames and show these.
-# Kim S. Pedersen, 2023
-
 from picamera2 import Picamera2
 import time
 picam2 = Picamera2()
-capture_config = picam2.create_preview_configuration
-picam2.start(show_preview=False)
-
+picam2.start()
 time.sleep(1)
-picam2.switch_mode_and_capture_file(capture_config, "testfocal.jpg")
+array = picam2.capture_array("main")
