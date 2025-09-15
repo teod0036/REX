efa17bdc99a668b424268a055573e2e2a9369c15
Andreas Kærlev <bzd796@alumni.ku.dk>
2025-09-15 16:22:14 +0200

    updated capture

--- Ex-3/imagecapture.py
+++ Ex-3/imagecapture.py
@@ -4,7 +4,8 @@
 from picamera2 import Picamera2
 import time
 picam2 = Picamera2()
-capture_config = picam2.create_still_configuration()
-picam2.start(show_preview=True)
+capture_config = picam2.create_preview_configuration
+picam2.start(show_preview=False)
+
 time.sleep(1)
 picam2.switch_mode_and_capture_file(capture_config, "testfocal.jpg")
