b6dbc26b01d02446ea378182b958d6b6aaac5342
teod0036 <73177141+teod0036@users.noreply.github.com>
2025-09-15 16:37:18 +0200

    updated image capture code

--- Ex-3/imagecapture.py
+++ Ex-3/imagecapture.py
@@ -1,6 +1,49 @@
-from picamera2 import Picamera2
+# This script shows how to open a camera the picamera2 module and grab frames and show these.
+# Kim S. Pedersen, 2023
+
+import cv2 # Import the OpenCV library
 import time
-picam2 = Picamera2()
-picam2.start()
-time.sleep(1)
-array = picam2.capture_array("main")
+from pprint import *
+
+try:
+    import picamera2
+    print("Camera.py: Using picamera2 module")
+except ImportError:
+    print("Camera.py: picamera2 module not available")
+    exit(-1)
+
+
+
+
+print("OpenCV version = " + cv2.__version__)
+
+# Open a camera device for capturing
+imageSize = (1640, 1232)
+cam = picamera2.Picamera2()
+# Change configuration to set resolution, framerate
+''''''
+picam2_config = cam.create_still_configuration({"size": imageSize, "format": 'RGB888'},
+                                                            controls={"ScalerCrop": (0,0,3280,2464)},
+                                                            queue=False)
+cam.configure(picam2_config) # Not really necessary
+cam.start(show_preview=False)
+
+pprint(cam.camera_configuration()) # Print the camera configuration in use
+
+time.sleep(1)  # wait for camera to setup
+
+
+# Open a window
+#WIN_RF = "Example 1"
+#cv2.namedWindow(WIN_RF)
+#cv2.moveWindow(WIN_RF, 100, 100)
+
+
+
+image = cam.capture_file("image.jpeg")
+    
+# Show frames
+#cv2.imshow(WIN_RF, image)
+    
+
+# Finished successfully
