df3b27e75b8ff01e8df846976e31ffcb1491400a
teod0036 <73177141+teod0036@users.noreply.github.com>
2025-09-17 15:28:06 +0200

    updated MoveToLandmark to reflect changes to FindLandmark

--- Ex-3/MoveToLandmark.py
+++ Ex-3/MoveToLandmark.py
@@ -9,16 +9,16 @@ def go_to_landmark(target_landmark):
     while (True):
         maybe_landmark = perform_Findlandmark()
         if maybe_landmark is not None:
-            rvec, tvec = maybe_landmark
+            tvecs = maybe_landmark
         else:
             perform_Turn90(True, 0.1735)
 
-        if tvec[0] < -0.00001:
+        if tvecs[0] < -0.00001:
             perform_Turn90(True, 0)
-        elif tvec[0] > 0.00001:
+        elif tvecs[0] > 0.00001:
             perform_Turn90(False, 0)
         else:
-            if tvec[2] < 0.001:
+            if tvecs[2] < 0.001:
                 print(arlo.stop())
                 return
             print(arlo.go_diff(64, 64, 1, 1))
