b8181ff7b640f10ccecc011d545a82fd546c5b77
abxh <83485102+abxh@users.noreply.github.com>
2025-09-17 11:51:36 +0200

    show detected stuff?

--- Ex-3/FindLandmark.py
+++ Ex-3/FindLandmark.py
@@ -1,12 +1,14 @@
+import datetime
 from time import sleep
-import robot
-from Turn90 import perform_Turn90
-from imagecapture import takePicture
+
 import cv2
 
-
+import robot
+from imagecapture import takePicture
+from Turn90 import perform_Turn90
 
 arlo = robot.Robot()
+
 
 def perform_Findlandmark():
     print("FindLandmark.py: Taking a picture")
@@ -19,10 +21,29 @@ def perform_Findlandmark():
     dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
     print("FindLandmark.py: Attempting to detect Markers")
 
-    corners,ids,rejectedimgpoints = cv2.aruco.detectMarkers(image,dict)
+    corners, ids, rejectedimgpoints = cv2.aruco.detectMarkers(image, dict)
 
-    if (ids is not None):
+    for cnt in corners:
+        x, y, w, h = cv2.boundingRect(cnt)
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.putText(
+            image, f"{w} x {h}", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2
+        )
+    dt = datetime.datetime.now()
+    cv2.imwrite(f"{dt.strftime('%M%S')}.jpeg", image)
+
+    if ids is not None:
         for i in ids:
             print("FindLandmark.py: Found a landmark")
+
+    for cnt in corners:
+        x, y, w, h = cv2.boundingRect(cnt)
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.putText(
+            image, f"{w} x {h}", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2
+        )
+    dt = datetime.datetime.now()
+    cv2.imwrite(f"{dt.strftime('%M%S')}.jpeg", image)
+
 
 perform_Findlandmark()
