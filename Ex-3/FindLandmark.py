879587c008e71969b7a4ad0b1e861383042b0e2e
abxh <83485102+abxh@users.noreply.github.com>
2025-09-17 11:52:59 +0200

    weird stuff fix

--- Ex-3/FindLandmark.py
+++ Ex-3/FindLandmark.py
@@ -23,15 +23,6 @@ def perform_Findlandmark():
 
     corners, ids, rejectedimgpoints = cv2.aruco.detectMarkers(image, dict)
 
-    for cnt in corners:
-        x, y, w, h = cv2.boundingRect(cnt)
-        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv2.putText(
-            image, f"{w} x {h}", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2
-        )
-    dt = datetime.datetime.now()
-    cv2.imwrite(f"{dt.strftime('%M%S')}.jpeg", image)
-
     if ids is not None:
         for i in ids:
             print("FindLandmark.py: Found a landmark")
