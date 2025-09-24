4ba4735fe32e9c4da8cd4061d5767437036c3601
teod0036 <73177141+teod0036@users.noreply.github.com>
2025-09-24 10:37:13 +0200

    updated landmark moving to logic

--- Ex-3/MoveToLandmark.py
+++ Ex-3/MoveToLandmark.py
@@ -13,6 +13,7 @@ def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 def go_to_landmark(target_landmark):
+    last_turn_direction = True
     while (True):
         landmarks = arlo_master.perform_image_analysis_table()
         
@@ -21,16 +22,16 @@ def go_to_landmark(target_landmark):
             print(arlo.stop())
             continue
 
-        if landmarks[target_landmark].tvecs[0] < -0.05:
+        if landmarks[target_landmark].tvec[0] < -0.05:
+            last_turn_direction = True
             perform_Turn90(False, 0.1)
             print(arlo.go_diff(64, 64 + rightSpeedModifier[64], 1, 1))
-            print(arlo.stop())
-        elif landmarks[target_landmark].tvecs[0] > 0.05:
+        elif landmarks[target_landmark].tvec[0] > 0.05:
+            last_turn_direction = False
             perform_Turn90(True, 0.1)
             print(arlo.go_diff(64, 64 + rightSpeedModifier[64], 1, 1))
-            print(arlo.stop())
         else:
-            if landmarks[target_landmark].tvecs[2] < 0.1:
+            if landmarks[target_landmark].tvec[2] < 0.1:
                 print(arlo.stop())
                 return
             print(arlo.go_diff(64, 64 + rightSpeedModifier[64], 1, 1))
