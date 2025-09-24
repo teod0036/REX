a30b9153da87d8b3e717d086b5e625fe4b62dc33
teod0036 <73177141+teod0036@users.noreply.github.com>
2025-09-24 10:44:53 +0200

    made robot stop when close to landmark

--- Ex-3/MoveToLandmark.py
+++ Ex-3/MoveToLandmark.py
@@ -18,21 +18,22 @@ def go_to_landmark(target_landmark):
         landmarks = arlo_master.perform_image_analysis_table()
         
         if target_landmark not in landmarks: 
-            perform_Turn90(True, 0.1)
+            perform_Turn90(True, 0.05)
             print(arlo.stop())
             continue
 
         if landmarks[target_landmark].tvec[0] < -0.05:
             last_turn_direction = True
-            perform_Turn90(False, 0.1)
+            perform_Turn90(False, 0.05)
             print(arlo.go_diff(64, 64 + rightSpeedModifier[64], 1, 1))
         elif landmarks[target_landmark].tvec[0] > 0.05:
             last_turn_direction = False
-            perform_Turn90(True, 0.1)
+            perform_Turn90(True, 0.05)
             print(arlo.go_diff(64, 64 + rightSpeedModifier[64], 1, 1))
         else:
-            if landmarks[target_landmark].tvec[2] < 0.1:
+            if landmarks[target_landmark].tvec[2] < 0.15 or arlo.read_front_ping_sensor() < 150:
                 print(arlo.stop())
+                eprint(f"I have arrived at landmark {target_landmark}")
                 return
             print(arlo.go_diff(64, 64 + rightSpeedModifier[64], 1, 1))
 
