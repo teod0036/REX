a084e8707c9c7281eedfd1ea88ec71551f248d25
teod0036 <73177141+teod0036@users.noreply.github.com>
2025-09-24 13:32:51 +0200

    improved visualization logic

--- Ex-3/SimpleMap.py
+++ Ex-3/SimpleMap.py
@@ -22,24 +22,31 @@ def get_map():
 def gridize_map(landmark_coordinates):
     map_size = 13
     grid_map = np.zeros((map_size, map_size), dtype=int)
-    grid_map[int((map_size-1)/2)][int((map_size-1)/2)] = 99
+    grid_map[0][int((map_size-1)/2)] = 99
 
     for mark in landmark_coordinates:
-        x = int(np.round(mark[1]*100/20)) + 6
-        y = int(np.round(mark[2]*100/20)) + 6
+        x = int(np.round(mark[1]*100/20)) + int((map_size-1)/2)
+        y = int(np.round(mark[2]*100/20)) + int((map_size-1)/2)
         mark_id = mark[0]
         if x > 12:
             x = 12
+            mark_id *= -1
+        elif x < 0:
+            x = 0
             mark_id *= -1
         if y > 12:
             y = 12
             if mark_id > 0:
                 mark_id *= -1 
+        elif y < 0:
+            y = 0
+            if mark_id > 0:
+                mark_id *= -1
         
         eprint(f"landmark {mark_id} is at ({mark[1]},{mark[2]}) in world")
         eprint(f"landmark {mark_id} is at ({x},{y}) in map")
         grid_map[y][x] = int(mark_id)
-        eprint(grid_map)
+
 
     eprint(grid_map)
 
