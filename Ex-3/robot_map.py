643450956cb1640d02ec489c9b3a86db4f4175e3
abxh <83485102+abxh@users.noreply.github.com>
2025-09-24 14:26:12 +0200

    gah

--- Ex-3/robot_map.py
+++ Ex-3/robot_map.py
@@ -17,8 +17,9 @@ def create_map(markers: List[Marker]):
     LOD = np.ceil(0.5 * np.log2(2 * max_extent_m / base_resolution))
     resolution = base_resolution * (2**LOD)  # meter / cell
 
-    print(f"{LOD = }")
-    print(f"base_resolution * (2**LOD) = {resolution = } m")
+    eprint(f"{base_resolution = } m")
+    eprint(f"{LOD = }")
+    eprint(f"base_resolution * (2**LOD) = {resolution = } m")
 
     map_size = max(1, int(np.ceil((2 * max_extent_m) / resolution)))
     map_array = np.full((map_size, map_size), 0, dtype=np.int32)
