fb385cb847e64f821133bda6cd7d717e4d894650
teod0036 <73177141+teod0036@users.noreply.github.com>
2025-09-25 17:40:26 +0200

    added support for arlo driving a path

--- Ex-3/rrt_arlo/arlo_path.py
+++ Ex-3/rrt_arlo/arlo_path.py
@@ -7,5 +7,14 @@ def path_to_arlo_instructions(path, current_dir):
         target_dir = [point[0] - current_point[0], point[1] - current_point[1]]
         dot_prod = current_dir[0] * target_dir[0] + current_dir[1] * target_dir[1]
         cross_prod = current_dir[0] * target_dir[1] + current_dir[1] * target_dir[0]
-        pass
+        rot_deg = (180/math.pi) * math.atan2(cross_prod, dot_prod)
+        rot_dir = (rot_deg < 0)
+        instructions.append(["turn", (rot_dir, round(abs(rot_deg), 2))]) 
+
+        point_dist = math.sqrt((current_dir[0]-target_dir[0])**2 + (current_dir[1]-target_dir[1])**2)
+        instructions.append(["forward", round(point_dist, 2)])
+
+        current_dir = target_dir
+        current_point = point
+
     return instructions
