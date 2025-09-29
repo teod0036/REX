2750fd477191db32a161a17c206a6d40f790ae11
teod0036 <73177141+teod0036@users.noreply.github.com>
2025-09-29 15:38:58 +0200

    fixed bug

--- Ex-3/rrt_arlo/exec_arlo_instructions.py
+++ Ex-3/rrt_arlo/exec_arlo_instructions.py
@@ -9,6 +9,6 @@ def exec_instructions(arlo_instructions):
 if __name__ == '__main__':
     path = [[0, 1.9], [-0.11842211,  1.75475859], [-0.24480625,  1.59975199], [-0.3711904 ,  1.44474538], [-0.54398795,  1.35659427], [-0.6791863 ,  1.20921243], [-0.61288822,  1.02052072], [-0.59856512,  0.82103425], [-0.60073514,  0.62104603], [-0.40089634,  0.61301759], [-0.26021383,  0.47086123], [-0.11412401,  0.3342678], [0.03176616, 0.19746116], [0, 0]]
     path.reverse()
-    smooth_path = rdp.rdp(path, 0.05*2)
+    smooth_path = rdp(path, 0.05*2)
     instructions = path_to_arlo_instructions(smooth_path, [0, 1])
     exec_instructions(instructions)
