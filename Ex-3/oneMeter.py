b5e07bca08a3daac035c258ce7537e7da85d31f4
abxh <83485102+abxh@users.noreply.github.com>
2025-10-01 14:39:00 +0200

    calibrated

---
+++ Ex-3/oneMeter.py
@@ -0,0 +1,17 @@
+from time import sleep
+import robot
+
+arlo = robot.Robot()
+
+def go_meter():
+    # Time constants
+    go_sleep = 2.3 + 1.65
+
+    # Speed constants
+    leftSpeed = 64
+    rightSpeed = 64
+    rightSpeedmodifier = 0
+
+    #go straight
+    print(arlo.go_diff(leftSpeed, rightSpeed + rightSpeedmodifier, 1, 1))
+    sleep(go_sleep) 
