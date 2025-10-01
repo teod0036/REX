63758ce10517eda2b43d209879f315be5781a05c
abxh <83485102+abxh@users.noreply.github.com>
2025-10-01 14:27:05 +0200

    retune turn90

--- Ex-3/Turn90.py
+++ Ex-3/Turn90.py
@@ -3,7 +3,7 @@ import robot
 
 arlo = robot.Robot()
 
-def perform_Turn90(withclock: bool, sleepduration: float = 0.694):
+def perform_Turn90(withclock: bool, sleepduration: float = 0.694 + 0.2):
     # Speed constants
     leftSpeed = 64  
     rightSpeed = 64 - 3
@@ -11,6 +11,7 @@ def perform_Turn90(withclock: bool, sleepduration: float = 0.694):
         print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
     else:
         print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
+        sleepduration += 0.05
 
     sleep(sleepduration)
 
