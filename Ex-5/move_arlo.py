cfd04999bc79c162dd76b6020c28ca0dbd32141e
teod0036 <73177141+teod0036@users.noreply.github.com>
2025-10-05 16:46:31 +0200

    added some functionality that will become helpful later

---
+++ Ex-5/move_arlo.py
@@ -0,0 +1,33 @@
+from time import sleep
+import robot
+
+arlo = robot.Robot()
+
+def turn(params):
+    withclock, degrees = params
+    # Speed constants
+    leftSpeed = 64  
+    rightSpeed = 64 - 3
+    extraconst_c = 0.2
+    extraconst_nc = 0.25
+    if withclock:
+        print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
+        sleep((0.694 + extraconst_c) * degrees/90)
+    else:
+        print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
+        sleep((0.694 + extraconst_nc) * degrees/90)
+
+
+def forward(distance):
+    # Time constants
+    extraconst = 1.65
+    go_sleep = (2.3 + extraconst) * distance
+
+    # Speed constants
+    leftSpeed = 64
+    rightSpeed = 64
+    rightSpeedmodifier = 0
+
+    #go straight
+    print(arlo.go_diff(leftSpeed, rightSpeed + rightSpeedmodifier, 1, 1))
+    sleep(go_sleep)
