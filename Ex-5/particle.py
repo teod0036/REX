3cf21e03f102cac0c9ae000fa0297e303e6c77db
teod0036 <73177141+teod0036@users.noreply.github.com>
2025-10-06 13:36:49 +0200

    refactored the particle update code to use the move_particle() function

--- Ex-5/particle.py
+++ Ex-5/particle.py
@@ -64,7 +64,10 @@ def estimate_pose(particles_list):
      
 def move_particle(particle, delta_x, delta_y, delta_theta):
     """Move the particle by (delta_x, delta_y, delta_theta)"""
-    print("particle.py: move_particle not implemented. You should do this.") 
+    particle.setTheta(particle.getTheta()+delta_theta)
+    particle.setX(particle.getX() + delta_x)
+    particle.setY(particle.getY() + delta_y)
+    return particle
 
 
 def add_uncertainty(particles_list, sigma, sigma_theta):
