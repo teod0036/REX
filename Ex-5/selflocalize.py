4bee7f9ef711382b45c5f43a8e03c775728282a8
teod0036 <73177141+teod0036@users.noreply.github.com>
2025-10-20 14:25:30 +0200

    disabled movement other than rotation

--- Ex-5/selflocalize.py
+++ Ex-5/selflocalize.py
@@ -292,7 +292,7 @@ if __name__ == "__main__":
                 #remove instructions exceeding the maximum.
                 if maxinstructions_per_execution is not None:
                     instructions = instructions[:maxinstructions_per_execution]
-                
+                instructions = []
                 #Calculate how far the robot is from it's goal.
                 #This value is used to check whether the robot has arrived or not.
                 #The distance is in meters.
