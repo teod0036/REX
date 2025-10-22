91e06c3cd4c77502728060f6002762b0fd684ec8
teod0036 <73177141+teod0036@users.noreply.github.com>
2025-10-22 17:04:21 +0200

    fixed test logic

--- Ex-5/exec_arlo_instructions.py
+++ Ex-5/exec_arlo_instructions.py
@@ -1,11 +1,12 @@
 import move_arlo
 
-def all(arlo_instructions, rm=True):
+def all(arlo_instructions):
     for i in range(len(arlo_instructions)):
-        next(arlo_instructons=arlo_instructions, rm=rm)
+        next(arlo_instructons=arlo_instructions)
 
 def next(arlo_instructons, rm=True):
     distance_driven = getattr(move_arlo, arlo_instructons[0][0])(arlo_instructons[0][1])
+    print(distance_driven)
     if distance_driven != 0:
         del arlo_instructons[1:]
         if distance_driven < 0:
@@ -25,6 +26,7 @@ def next(arlo_instructons, rm=True):
             arlo_instructons.append(["turn", (False, 90)])
             arlo_instructons.append(["forward", (0.5)])
             arlo_instructons.append(["turn", (True, 90)])
+
         arlo_instructons[0][1] = distance_driven
 
     if rm:
@@ -33,3 +35,4 @@ def next(arlo_instructons, rm=True):
 if __name__ == "__main__":
     instructions = [["forward", (3)], ["forward", (0.5)]]
     next(instructions, rm=False)
+    all(instructions)
