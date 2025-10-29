8afd27b426795ee6451843e12c56baf32fe81d26
teod0036 <73177141+teod0036@users.noreply.github.com>
2025-10-29 14:16:23 +0100

    fixed debug flags

--- Ex-Final/selflocalize.py
+++ Ex-Final/selflocalize.py
@@ -9,9 +9,9 @@ import camera
 import particle
 
 # Flags
-onRobot = False  # Whether or not we are running on the Arlo robot
+onRobot = True  # Whether or not we are running on the Arlo robot
 showGUI = True  # Whether or not to open GUI windows
-instruction_debug = True  # Whether you want to debug the isntrcution execution code, even if you don't have an arlo
+instruction_debug = False  # Whether you want to debug the isntrcution execution code, even if you don't have an arlo
 
 
 def isRunningOnArlo():
